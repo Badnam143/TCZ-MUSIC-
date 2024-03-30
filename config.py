68cb6e46908893883f8e1b1f47231229268c944a
Badnam143 <154507591+Badnam143@users.noreply.github.com>
2024-03-30 11:37:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Badnam143/Blackymusicuu",
+    "https://github.com/Badnam143/TCZ-MUSIC-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
